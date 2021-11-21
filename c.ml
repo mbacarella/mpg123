ea7b84ce26f9dd3d76dc4eed46177b8ee1798fc7
Michael Bacarella <m@bacarella.com>
2021-11-21 09:28:01 -0800

    .ocamlformat

--- c.ml
+++ c.ml
@@ -1,4 +1,2 @@
 module Types = Mpg123_c.Mpg123_c_types
-
-module Functions =
-  Mpg123_c_function_descriptions.Functions (Mpg123_c.Mpg123_c_generated_functions)
+module Functions = Mpg123_c_function_descriptions.Functions (Mpg123_c.Mpg123_c_generated_functions)
